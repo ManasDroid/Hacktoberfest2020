9fd79f9d3ac0a175f4f4174adee32bdb1df868d5
ARsh-ALgo <72185264+ARsh-ALgo@users.noreply.github.com>
2020-10-04 16:21:48 +0530

    Update Prime.c
    Added :-
    - (clrscr) to clear the console screen.
    - (getch) to directly read character from the keyboard.

--- CODE in C/Numbers/Prime.c
+++ CODE in C/Numbers/Prime.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include<conio.h>
 int main() {
     int n, i, flag = 0;
+    clrscr();
     printf("Enter a positive integer: ");
     scanf("%d", &n);
 
@@ -23,5 +25,5 @@ int main() {
             printf("%d is not a Prime Number", n);
     }
 
-    return 0;
+    getch();
 }
